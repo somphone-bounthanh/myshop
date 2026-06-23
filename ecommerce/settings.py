cf603c58a821c50ca9fb2fe154c3567e59bbe831
unknown <noy@gmail.com>
2026-06-23 13:24:26 +0700

    first commit10

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -133,9 +133,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[STATIC_DIR,]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR,]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
