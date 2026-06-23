93c27ea39cbeb980b0e638f516b18e390c996695
unknown <noy@gmail.com>
2026-06-23 14:42:06 +0700

    first commit11

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -146,9 +146,7 @@ STATICFILES_DIRS = [STATIC_DIR,]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL='/afterlogin'
 
